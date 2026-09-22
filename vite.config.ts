4faef60294e22ef0bda7e407a75c83ec8adcd0a1
HAYEKCODING <diabagatem28@gmail.com>
2026-09-22 15:13:52 -0300

    Configure portfolio for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
 
@@ -17,6 +18,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
+    netlify(),
   ],
 
   resolve: {
